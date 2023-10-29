bb2f9177121d12a9947c965774f6646f001a9305
Alexeykaa <ka.alexey@gmail.com>
2023-10-29 18:45:55 +0300

    init implementation

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,8 @@
-select * from student;
+ALTER TABLE mark DROP CONSTRAINT mark_student_id_fkey RESTRICT;
+ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey foreign key (student_id) references student(id) ON DELETE CASCADE;
+ALTER TABLE mark DROP CONSTRAINT mark_subject_id_fkey RESTRICT;
+ALTER TABLE mark ADD CONSTRAINT mark_subject_id_fkey foreign key (subject_id) references subject(id) ON DELETE CASCADE;
+ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey RESTRICT;
+ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey foreign key (student_id) references student(id) ON DELETE CASCADE;
+ALTER TABLE payment DROP CONSTRAINT payment_type_id_fkey RESTRICT;
+ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey foreign key (type_id) references paymenttype(id) ON DELETE CASCADE;
